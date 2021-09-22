3057205db08b74763aef1744ae253a91c6dee3c1
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2021-09-22 23:00:14 +0200

    Delete old project

--- SimpleLogin/app/src/main/java/io/simplelogin/android/utils/extension/Fragment.kt
+++
@@ -1,7 +0,0 @@
-package io.simplelogin.android.utils.extension
-
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-
-fun Fragment.copyToClipboard(label: String, text: String) : Boolean =
-    (activity as? AppCompatActivity)?.copyToClipboard(label, text) ?: false
